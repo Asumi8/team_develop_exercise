e61ae94afd35cb2aa4bdd30834d5d8e94030022a
asumi nakajima <nakajima0848@gmail.com>
2022-11-14 14:53:05 +0900

    チームのオーナー権限の移動の設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy change_owner]
 
   def index
     @teams = Team.all
@@ -45,6 +45,12 @@ class TeamsController < ApplicationController
 
   def dashboard
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  def change_owner
+    @team.update(owner_id: params[:owner_id])
+    @user = User.find(@team.owner_id)
+    redirect_to team_path, notice: "権限を変更しました"
   end
 
   private
